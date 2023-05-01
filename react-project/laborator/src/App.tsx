a745721d1c83826fbfd176ddf2654fc248d0872a
RodnitchiAnghelina <124936080+RodnitchiAnghelina@users.noreply.github.com>
2023-05-01 20:52:33 +0300

    lab5

--- react-project/laborator/src/App.tsx
+++ react-project/laborator/src/App.tsx
@@ -1,55 +1,60 @@
-import { App } from 'antd';
 import React, { useState } from 'react';
+import { useStore } from './MyStore';
+import './index.css';
 
-interface Student {
-  name: string;
-  age: number;
-  studentId: string;
-  major: string;
-  courses: string[];
+function MyComponent() {
+    const store = useStore();
+
+    // local state
+    const [localData, setLocalData] = useState<any>({ nume: '', prenume: '',facultatea:'', grupa: '' });
+
+    // folosim datele din MobX store
+    const myData = store.myData;
+
+    // refresh la date
+    // adaugam un obiect nou  la MobX store
+    const handleAddObject = (event: React.FormEvent<HTMLFormElement>) => {
+
+        event.preventDefault();
+        
+        store.addObject(localData);
+        setLocalData({ nume: '', prenume: '', facultatea:'',grupa: '' });
+    };
+
+
+// stergem obiectul din MobX store si  facem update la  local state
+    const handleDeleteObject = (index: number) => {
+        store.deleteObject(index);
+        setLocalData({ nume: "", prenume: "",facultatea:"", grupa: "" });
+    };
+
+
+    return (
+
+        <div className="container">
+            <h1 className="title">Registru</h1>
+            <form className="form" onSubmit={handleAddObject}>
+                <input className="input" type="text" placeholder="Nume" value={localData.nume} onChange={e => setLocalData({ ...localData, nume: e.target.value })} />
+                <input className="input" type="text" placeholder="Prenume" value={localData.prenume} onChange={e => setLocalData({ ...localData, prenume: e.target.value })} />
+                <input className="input" type="text" placeholder="Facultatea" value={localData.facultatea} onChange={e => setLocalData({ ...localData, facultatea: e.target.value })} />
+                <input className="input" type="text" placeholder="Grupa" value={localData.grupa} onChange={e => setLocalData({ ...localData, grupa: e.target.value })} />
+                <button className="button" type="submit">Trimite</button>
+            </form>
+            <div>
+                <div className="grid">
+                    {myData.map((data: any, index: number) => (
+                        <div key={index} className="object">
+                            <p>Nume: {data.nume}</p>
+                            <p>Prenume: {data.prenume}</p>
+                            <p>Facultatea: {data.facultatea}</p>
+                            <p>Grupa: {data.grupa}</p>
+                            <button onClick={() => handleDeleteObject(index)}>Delete</button>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </div>
+    );
 }
 
-interface AdvancedStudent extends Student {
-  graduationDate: Date;
-  honors: boolean;
-}
-
-const student: Student = {
-  name: 'Rodnitchi Anghelina',
-  age: 21,
-  studentId: '12345',
-  major: 'FCIM',
-  courses: ['Java Fundamentals', 'TSI', 'C++']
-};
-
-function Lab4() {
-  return (
-    <div className="container">
-      <h1 className="title">Date Personale</h1>
-      <div className="card">
-        <p>
-          <span className="label">Nume:</span>
-          <span className="value">{student.name}</span>
-        </p>
-        <p>
-          <span className="label">Varsta:</span>
-          <span className="value">{student.age}</span>
-        </p>
-        <p>
-          <span className="label">StudentId:</span>
-          <span className="value">{student.studentId}</span>
-        </p>
-        <p>
-          <span className="label">Facultatea:</span>
-          <span className="value">{student.major}</span>
-        </p>
-        <p>
-          <span className="label">Cursuri:</span>
-          <span className="value">{student.courses.join(', ')}</span>
-        </p>
-      </div>
-    </div>
-  );
-}
-
-export default Lab4;
+export default MyComponent;

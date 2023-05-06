4b9924f37357b1de70d00c1bb30f2557ce50a987
RodnitchiAnghelina <124936080+RodnitchiAnghelina@users.noreply.github.com>
2023-05-06 22:48:00 +0300

    lab6

--- react-project/laborator/src/App.tsx
+++ react-project/laborator/src/App.tsx
@@ -1,182 +1,89 @@
-import React, { useState, useEffect } from 'react';
-import { useStore } from './MyStore';
-import './index.css';
+import React, { useState, useEffect } from "react";
 
-function MyComponent() {
-    const store = useStore();
+const UserInitial = [
+   
+    { username: 'Anghelina', password: '12a' },
+    { username: 'Ion', password: '23b' },
+    { username: 'Cristina', password: '1234ab' }
 
-    const [localData, setLocalData] = useState<any>({ nume: '', prenume: '',facultatea:'', grupa: '' });
-    const myData = store.myData;
+];
 
-    const [isEditing, setIsEditing] = useState(false);
-    const [editingIndex, setEditingIndex] = useState(-1);
+const App = () => {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [EsteLogat, setEsteLogat] = useState(false);
+    const [users, SetareUser] = useState(UserInitial);
 
-    const handleAddObject = (event: React.FormEvent<HTMLFormElement>) => {
+    useEffect(() => {
+        const storedUsers = localStorage.getItem("users");
+        if (storedUsers) {
+            SetareUser([...users, ...JSON.parse(storedUsers)]);
+        }
+    }, [users]);
+
+    const handleLogin = (event: { preventDefault: () => void; }) => {
         event.preventDefault();
-        if (!localData.nume || !localData.prenume || !localData.facultatea || !localData.grupa) {
-            alert('Introduceti toate campurile.');
-            return;
-        }
-
-        setIsLoading(true);
-        setTimeout(() => {
-            store.addObject(localData);
-            localStorage.setItem('myData', JSON.stringify(store.myData));
-            setIsLoading(false);
-        }, 2000);
-
-    };
-
-
-    const handleDeleteObject = (index: number) => {
-        store.deleteObject(index);
-        setLocalData({ nume: '', prenume: '',facultatea:'', grupa: '' });
-        localStorage.setItem('myData', JSON.stringify(store.myData));
-    };
-
-    const handleUpdateObject = (index: number, newData: any) => {
-        store.updateObject(index, newData);
-        setIsLoading(true);
-        setTimeout(() => {
-            localStorage.setItem('myData', JSON.stringify(store.myData));
-            setIsLoading(false);
-        }, 2000);
-
-        // setează valorile din obiectul myData în starea localData
-        const dataToUpdate = store.myData[index];
-        setLocalData({ nume: dataToUpdate.nume, prenume: dataToUpdate.prenume, facultatea: dataToUpdate.facultatea, grupa: dataToUpdate.grupa });
-        setIsEditing(false);
-        setEditingIndex(-1);
-    };
-
-    const handleEditObject = (index: number) => {
-        const dataToUpdate = store.myData[index];
-        setLocalData({ nume: dataToUpdate.nume, prenume: dataToUpdate.prenume, facultatea: dataToUpdate.facultatea, grupa: dataToUpdate.grupa });
-        setIsEditing(true);
-        setEditingIndex(index);
-    };
-
-    const handleSaveObject = () => {
-        if (editingIndex >= 0) {
-            handleUpdateObject(editingIndex, localData);
-        }
-    };
-
-    const [isLoading, setIsLoading] = useState(false);
-
-    useEffect(() => {
-        const data = localStorage.getItem('myData');
-        if (data) {
-            store.setMyData(JSON.parse(data));
+        const foundUser = users.find(
+            (user) => user.username === username && user.password === password
+        );
+        if (foundUser) {
+            setEsteLogat(true);
+            localStorage.setItem("EsteLogat", "true");
+            localStorage.setItem("user", JSON.stringify(foundUser));
+            alert("Autentificare cu succes");
         } else {
-            localStorage.setItem('myData', JSON.stringify(store.myData));
+            alert("Login eșuat");
         }
-    }, [store]);
+    };
 
-    useEffect(() => {
-        if (!isLoading) {
-            const data = localStorage.getItem('myData');
-            if (data) {
-                store.setMyData(JSON.parse(data));
-            }
-        }
-    }, [isLoading, store]);
 
-    return (
+    const handleLogout = () => {
+        setEsteLogat(false);
+        localStorage.setItem("EsteLogat", "false");
+        alert("Succes la deconectare");
+    };
 
-        <div className="container">
-            <h1 className="title">Registru</h1>
-    
-              
-            <form className="form" onSubmit={handleAddObject}>
-                <input
-                    className="input"  type="text" placeholder="Nume" defaultValue={localData.name} onChange={(e) => setLocalData({ ...localData, nume: e.target.value })}
-                />
-                <input
-                    className="input" type="text" placeholder="Prenume" defaultValue={localData.address} onChange={(e) => setLocalData({ ...localData, prenume: e.target.value })}
-                />
-                <input
-                    className="input" type="text" placeholder="Facultatea" defaultValue={localData.phone} onChange={(e) => setLocalData({ ...localData, facultatea: e.target.value })}
-                />
-                 <input
-                    className="input"  type="text" placeholder="Grupa" defaultValue={localData.name} onChange={(e) => setLocalData({ ...localData, grupa: e.target.value })}
-                />
-                <button className="button" type="submit">{isLoading ? 'Se incarca...' : 'Adauga'}</button>
-
+    const renderLoginForm = () => {
+        return (
+<div className="cont">
+            <form onSubmit={handleLogin}>
+                <label>
+                    Username:
+                    <input
+                        type="text"
+                        value={username}
+                        onChange={(event) => setUsername(event.target.value)}
+                    />
+                </label>
+                <br />
+                <label>
+                    Password:
+                    <input
+                        type="password"
+                        value={password}
+                        onChange={(event) => setPassword(event.target.value)}
+                    />
+                </label>
+                <br />
+                <button type="submit">Submit</button>
             </form>
-            <table className="table">
-                <thead>
-                <tr>
-                    <th>Nume</th>
-                    <th>Prenume</th>
-                    <th>Facultatea</th>
-                    <th>Grupa</th>
-                </tr>
-                </thead>
-                <tbody>
-                {myData.map((item: any, index: number) => (
-                    <tr key={index}>
-                        <td>{item.nume}</td>
-                        <td>{item.prenume}</td>
-                        <td>{item.facultatea}</td>
-                        <td>{item.grupa}</td>
-                        <td>
-                            <button className="button edit" onClick={() => handleEditObject(index)}>
-                                Editeaza
-                            </button>
-                            <button className="button delete" onClick={() => handleDeleteObject(index)}>
-                                Sterge
-                            </button>
-                        </td>
-                    </tr>
-                ))}
-                </tbody>
-            </table>
-            {isEditing && (
-                <div className="edit-form">
-                    <h2>Editare</h2>
-                    <form onSubmit={handleSaveObject}>
-                        <input
-                            className="input"
-                            type="text"
-                            placeholder="Nume"
-                            value={localData.nume}
-                            onChange={(e) => setLocalData({ ...localData, nume: e.target.value })}
-                        />
-                        <input
-                            className="input"
-                            type="text"
-                            placeholder="Prenume"
-                            value={localData.prenume}
-                            onChange={(e) => setLocalData({ ...localData, prenume: e.target.value })}
-                        />
-                        <input
-                            className="input"
-                            type="text"
-                            placeholder="Facultatea"
-                            value={localData.facultatea}
-                            onChange={(e) => setLocalData({ ...localData, facultatea: e.target.value })}
-                        />
-                        <input
-                            className="input"
-                            type="text"
-                            placeholder="Grupa"
-                            value={localData.grupa}
-                            onChange={(e) => setLocalData({ ...localData, grupa: e.target.value })}
-                        />
-                        <div className="edit-form-buttons">
-                            <button className="button save" type="submit">
-                                Salveaza
-                            </button>
-                            <button className="button cancel" onClick={() => setIsEditing(false)}>
-                                Anuleaza
-                            </button>
-                        </div>
-                    </form>
-                </div>
-            )}
-        </div>
-    );
-}
+            </div>
+        );
+    };
 
-export default MyComponent;
+    const renderLogoutButton = () => {
+        const storedUser = localStorage.getItem("user");
+        const loggedInUser = storedUser ? JSON.parse(storedUser) : null;
+        return (
+            <div>
+                {loggedInUser && <p>User : {loggedInUser.username}</p>}
+                <button onClick={handleLogout}>EXIT</button>
+            </div>
+        );
+    };
+
+
+    return <div>{EsteLogat ? renderLogoutButton() : renderLoginForm()}</div>;
+};
+
+export default App;

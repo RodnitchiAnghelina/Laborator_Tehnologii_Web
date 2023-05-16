26f4afe8d41076bc8454e90e972bfe946fb26f03
RodnitchiAnghelina <124936080+RodnitchiAnghelina@users.noreply.github.com>
2023-05-16 14:29:43 +0300

    lab7

--- react-project/laborator/src/App.tsx
+++ react-project/laborator/src/App.tsx
@@ -1,89 +1,128 @@
-import React, { useState, useEffect } from "react";
+import { BrowserRouter, Routes, Route, Link, useParams } from 'react-router-dom';
+import './App.css';
+import './styles.css';
 
-const UserInitial = [
-   
-    { username: 'Anghelina', password: '12a' },
-    { username: 'Ion', password: '23b' },
-    { username: 'Cristina', password: '1234ab' }
+function Home() {
+  return (
+    <div>
+      <header>
+        <h1>Travel App</h1>
+      </header>
+      <section>
+        <h2>Home</h2>
+        <p>Welcome to our travel app!</p>
+      </section>
+    </div>
+  );
+}
 
-];
+function About() {
+  return (
+    <div>
+      <header>
+        <h1>Travel App</h1>
+      </header>
+      <section>
+        <h2>About</h2>
+        <p>Our travel app helps you plan your next adventure!</p>
+      </section>
+    </div>
+  );
+}
 
-const App = () => {
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
-    const [EsteLogat, setEsteLogat] = useState(false);
-    const [users, SetareUser] = useState(UserInitial);
+function Contact() {
+  return (
+    <div>
+      <header>
+        <h1>Travel App</h1>
+      </header>
+      <section>
+        <h2>Contact</h2>
+        <p>Get in touch with us:</p>
+        <ul>
+          <li>Email: contact@travelapp.com</li>
+          <li>Phone: 555-1234</li>
+        </ul>
+      </section>
+    </div>
+  );
+}
 
-    useEffect(() => {
-        const storedUsers = localStorage.getItem("users");
-        if (storedUsers) {
-            SetareUser([...users, ...JSON.parse(storedUsers)]);
-        }
-    }, [users]);
+function Destinations() {
+  const destinations = [
+    { id: '1', name: 'Paris', description: 'The City of Light', image: 'https://travellersworldwide.com/wp-content/uploads/2022/06/shutterstock_667548661.png.webp' },
+    { id: '2', name: 'Tokyo', description: 'A vibrant city with something for everyone', image: 'https://assets.editorial.aetnd.com/uploads/2013/07/gettyimages-1390815938.jpg' },
+    { id: '3', name: 'New York', description: 'The Big Apple', image: 'https://www.offsoho.com/images/NYC_000021208828-2100-980.jpg' },
+  ];
+  
 
-    const handleLogin = (event: { preventDefault: () => void; }) => {
-        event.preventDefault();
-        const foundUser = users.find(
-            (user) => user.username === username && user.password === password
-        );
-        if (foundUser) {
-            setEsteLogat(true);
-            localStorage.setItem("EsteLogat", "true");
-            localStorage.setItem("user", JSON.stringify(foundUser));
-            alert("Autentificare cu succes");
-        } else {
-            alert("Login eșuat");
-        }
-    };
+  return (
+    <div className="destinations">
+      <header>
+        <h1>Travel App</h1>
+      </header>
+      <section>
+        <h2>Destinations</h2>
+        <ul>
+          {destinations.map((destination) => (
+            <li key={destination.id} style={{ backgroundImage: `url(${destination.image})` }}>
+              <Link to={`/destination/${destination.id}`}>
+                {destination.name}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </section>
+    </div>
+  );
+  
+          }  
 
+function DestinationDetail() {
+  const { id } = useParams<{ id: string }>();
+  const destinations = [
+    { id: '1', name: 'Paris', description: 'The City of Light' },
+    { id: '2', name: 'Tokyo', description: 'A vibrant city with something for everyone' },
+    { id: '3', name: 'New York', description: 'The Big Apple' },
+  ];
+  const destination = destinations.find((destination) => destination.id === id);
 
-    const handleLogout = () => {
-        setEsteLogat(false);
-        localStorage.setItem("EsteLogat", "false");
-        alert("Succes la deconectare");
-    };
+  if (!destination) {
+    return <div className="destination-detail">Destination not found</div>;
+  }
 
-    const renderLoginForm = () => {
-        return (
-<div className="cont">
-            <form onSubmit={handleLogin}>
-                <label>
-                    Username:
-                    <input
-                        type="text"
-                        value={username}
-                        onChange={(event) => setUsername(event.target.value)}
-                    />
-                </label>
-                <br />
-                <label>
-                    Password:
-                    <input
-                        type="password"
-                        value={password}
-                        onChange={(event) => setPassword(event.target.value)}
-                    />
-                </label>
-                <br />
-                <button type="submit">Submit</button>
-            </form>
-            </div>
-        );
-    };
-
-    const renderLogoutButton = () => {
-        const storedUser = localStorage.getItem("user");
-        const loggedInUser = storedUser ? JSON.parse(storedUser) : null;
-        return (
-            <div>
-                {loggedInUser && <p>User : {loggedInUser.username}</p>}
-                <button onClick={handleLogout}>EXIT</button>
-            </div>
-        );
-    };
-
-
-    return <div>{EsteLogat ? renderLogoutButton() : renderLoginForm()}</div>;
-};
-
-export default App;
+  return (
+    <div className="destination-detail">
+    <header>
+    <h1>{destination.name}</h1>
+    </header>
+    <section>
+    <p>{destination.description}</p>
+    </section>
+    </div>
+    );
+    }
+    
+    function App() {
+    return (
+    <BrowserRouter>
+    <nav>
+    <ul>
+    <li><Link to="/">Home</Link></li>
+    <li><Link to="/about">About</Link></li>
+    <li><Link to="/destinations">Destinations</Link></li>
+    <li><Link to="/contact">Contact</Link></li>
+    </ul>
+    </nav>
+    <Routes>
+    <Route path="/" element={<Home />} />
+    <Route path="/about" element={<About />} />
+    <Route path="/destinations" element={<Destinations />} />
+    <Route path="/destination/:id" element={<DestinationDetail />} />
+    <Route path="/contact" element={<Contact />} />
+    </Routes>
+    </BrowserRouter>
+    );
+    }
+    
+    export default App;
